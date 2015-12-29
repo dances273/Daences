020b0f740c784b4627036f715938c556a5d8387c
Bodnár Zoltán <dances273@gmail.com>
2015-12-29 22:39:22 +0100

    Tessék a program remélem sikerül így, ha nem akkor elküldöm e-mailben is.

---
+++ Progi/Kasse/Kasse/Product_reg.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Kasse
+{
+    partial class Product_reg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.Barcode = new System.Windows.Forms.TextBox();
+            this.ProductName = new System.Windows.Forms.TextBox();
+            this.Quantity = new System.Windows.Forms.TextBox();
+            this.Brutto = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Szallito = new System.Windows.Forms.TextBox();
+            this.Datum = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Netto = new System.Windows.Forms.TextBox();
+            this.Felnott = new System.Windows.Forms.CheckBox();
+            this.ID = new System.Windows.Forms.Label();
+            this.IDcode = new System.Windows.Forms.TextBox();
+            this.Fajlutvonal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButton1.Location = new System.Drawing.Point(26, 212);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "27%";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(95, 212);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "18%";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(156, 212);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(39, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "5%";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Barcode
+            // 
+            this.Barcode.Location = new System.Drawing.Point(95, 47);
+            this.Barcode.Name = "Barcode";
+            this.Barcode.Size = new System.Drawing.Size(100, 20);
+            this.Barcode.TabIndex = 3;
+            // 
+            // ProductName
+            // 
+            this.ProductName.Location = new System.Drawing.Point(95, 77);
+            this.ProductName.Name = "ProductName";
+            this.ProductName.Size = new System.Drawing.Size(100, 20);
+            this.ProductName.TabIndex = 4;
+            // 
+            // Quantity
+            // 
+            this.Quantity.Location = new System.Drawing.Point(95, 105);
+            this.Quantity.Name = "Quantity";
+            this.Quantity.Size = new System.Drawing.Size(100, 20);
+            this.Quantity.TabIndex = 5;
+            // 
+            // Brutto
+            // 
+            this.Brutto.Location = new System.Drawing.Point(95, 134);
+            this.Brutto.Name = "Brutto";
+            this.Brutto.Size = new System.Drawing.Size(100, 20);
+            this.Brutto.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Fájl betöltő";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Vonalkód";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Név";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Mennyiseg";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Bruttó Ár";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Szállító";
+            // 
+            // Szallito
+            // 
+            this.Szallito.Location = new System.Drawing.Point(95, 160);
+            this.Szallito.Name = "Szallito";
+            this.Szallito.Size = new System.Drawing.Size(100, 20);
+            this.Szallito.TabIndex = 13;
+            // 
+            // Datum
+            // 
+            this.Datum.Location = new System.Drawing.Point(95, 186);
+            this.Datum.Name = "Datum";
+            this.Datum.Size = new System.Drawing.Size(100, 20);
+            this.Datum.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Dátum";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 242);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Nettó Ár";
+            // 
+            // Netto
+            // 
+            this.Netto.Location = new System.Drawing.Point(95, 235);
+            this.Netto.Name = "Netto";
+            this.Netto.Size = new System.Drawing.Size(100, 20);
+            this.Netto.TabIndex = 17;
+            // 
+            // Felnott
+            // 
+            this.Felnott.AutoSize = true;
+            this.Felnott.Location = new System.Drawing.Point(26, 268);
+            this.Felnott.Name = "Felnott";
+            this.Felnott.Size = new System.Drawing.Size(95, 17);
+            this.Felnott.TabIndex = 19;
+            this.Felnott.Text = "Felnőt tartalom";
+            this.Felnott.UseVisualStyleBackColor = true;
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(26, 29);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(18, 13);
+            this.ID.TabIndex = 20;
+            this.ID.Text = "ID";
+            // 
+            // IDcode
+            // 
+            this.IDcode.Location = new System.Drawing.Point(95, 21);
+            this.IDcode.Name = "IDcode";
+            this.IDcode.Size = new System.Drawing.Size(100, 20);
+            this.IDcode.TabIndex = 21;
+            // 
+            // Fajlutvonal
+            // 
+            this.Fajlutvonal.Location = new System.Drawing.Point(272, 55);
+            this.Fajlutvonal.Name = "Fajlutvonal";
+            this.Fajlutvonal.Size = new System.Drawing.Size(100, 23);
+            this.Fajlutvonal.TabIndex = 22;
+            // 
+            // Product_reg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 297);
+            this.Controls.Add(this.Fajlutvonal);
+            this.Controls.Add(this.IDcode);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.Felnott);
+            this.Controls.Add(this.Netto);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Datum);
+            this.Controls.Add(this.Szallito);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Brutto);
+            this.Controls.Add(this.Quantity);
+            this.Controls.Add(this.ProductName);
+            this.Controls.Add(this.Barcode);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Product_reg";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Product_reg";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox Barcode;
+        private System.Windows.Forms.TextBox ProductName;
+        private System.Windows.Forms.TextBox Quantity;
+        private System.Windows.Forms.TextBox Brutto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Szallito;
+        private System.Windows.Forms.TextBox Datum;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Netto;
+        private System.Windows.Forms.CheckBox Felnott;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.TextBox IDcode;
+        private System.Windows.Forms.Label Fajlutvonal;
+    }
+}
